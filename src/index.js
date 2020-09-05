a44fd7f6bb8c8eccc008302dee20e301ddb3b5b1
Umair Khan <umair304@gmail.com>
2020-09-05 20:10:49 +1000

    changed UI for resume page and reduced opacity of background

--- src/index.js
+++ src/index.js
@@ -7,11 +7,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
     <BrowserRouter >
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  ,
   document.getElementById('root')
 );
 
@@ -21,4 +20,3 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
